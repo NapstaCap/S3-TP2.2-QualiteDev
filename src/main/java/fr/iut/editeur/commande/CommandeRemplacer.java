8f79d4e2319d69c2dca7184a71a9ecf82b66f6cf
barnierd <dorian.barnier@etu.umontpellier.fr>
2024-09-03 14:50:32 +0200

    fix complet commandeRemplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -4,19 +4,34 @@ import fr.iut.editeur.document.Document;
 
 public class CommandeRemplacer extends CommandeDocument {
 
-    public CommandeRemplacer (Document document, String[] parameters) {
+    public CommandeRemplacer(Document document, String[] parameters) {
         super(document, parameters);
     }
 
     @Override
     public void executer() {
-        if(parameters.length < 4) {
+        String remplacement;
+        if (parameters.length < 3) {
             System.err.println("Format attendu : remplacer;debut;fin;remplacement");
             return;
         }
+        // VERSION FACILE
+        if (parameters.length == 3) {
+            remplacement = "";
+        } else {
+            remplacement = parameters[3];
+        }
+        // VERSION AVANCEE
+        /*
+        if (parameters.length < 4 || Objects.equals(parameters[3], "")) {
+            this.document.remplacer(debut, fin, "");
+            super.executer();
+        } else {
+        la suite...
+        }
+        */
         int debut = Integer.parseInt(parameters[1]);
         int fin = Integer.parseInt(parameters[2]);
-        String remplacement = parameters[3];
         this.document.remplacer(debut, fin, remplacement);
         super.executer();
     }
