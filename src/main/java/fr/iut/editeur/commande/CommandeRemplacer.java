28db5f354f5aab258457eb3ae7cd3b990488e60b
barnierd <dorian.barnier@etu.umontpellier.fr>
2024-09-03 14:25:37 +0200

    # Ceci est la combinaison de 4 commits. # Ceci est le premier message de validation :
    fix bug commandeRemplacer
    
    # Ceci est le message de validation numéro 2 :
    
    fix complet commandeRemplacer
    
    # Ceci est le message de validation numéro 3 :
    
    modifications main, document, commandefactory
    
    # Ceci est le message de validation numéro 4 :
    
    ajout CommandeMajuscules, modifications commandeFactory, main

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -1,4 +1,38 @@
 package fr.iut.editeur.commande;
 
-public class CommandeRemplacer {
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        String remplacement;
+        if (parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;debut;fin;remplacement");
+            return;
+        }
+        // VERSION FACILE
+        if (parameters.length == 3) {
+            remplacement = "";
+        } else {
+            remplacement = parameters[3];
+        }
+        // VERSION AVANCEE
+        /*
+        if (parameters.length < 4 || Objects.equals(parameters[3], "")) {
+            this.document.remplacer(debut, fin, "");
+            super.executer();
+        } else {
+        la suite...
+        }
+        */
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        this.document.remplacer(debut, fin, remplacement);
+        super.executer();
+    }
 }
