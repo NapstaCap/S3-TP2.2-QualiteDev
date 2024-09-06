c81c4b0660f3e4817bc0b954cb70c7a6d3db5aaa
dorian barnier <dorian.barnier@etu.umontpellier.fr>
2024-09-06 08:07:10 +0200

    modifications main, document, commandefactory

--- src/main/java/fr/iut/editeur/commande/CommandeMajuscules.java
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscules.java
@@ -1,4 +1,22 @@
 package fr.iut.editeur.commande;
 
-public class CommandeMajuscules {
+import fr.iut.editeur.document.Document;
+
+public class CommandeMajuscules extends CommandeDocument {
+
+    public CommandeMajuscules(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if (parameters.length < 3) {
+            System.err.println("Format attendu : majuscules;debut;fin");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        this.document.majuscules(debut, fin);
+        super.executer();
+    }
 }
--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -33,4 +33,7 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    public void majuscules(int debut, int fin) {
+        remplacer(debut, fin, texte.substring(debut, fin+1).toUpperCase());
+    }
 }
